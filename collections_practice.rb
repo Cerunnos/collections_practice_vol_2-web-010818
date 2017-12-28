692ce43d242c8829d789e3930f4890d63ae84582
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-28 19:01:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(array)
+  true_to_false=true
+  array.each do |element|
+    true_to_false=false if element[0]!="r"
+  end
+  true_to_false
+end
+def contain_a(array)
+  elements_with_a=[]
+  array.each do |element|
+    if element.include?("a")
+      elements_with_a<<element
+    end
+  end
+  elements_with_a
+end
+def first_wa(array)
+  first_wa=nil
+  array.each do |element|
+    if element.match(/wa/)
+      first_wa=element
+      break
+    end
+  end
+  first_wa
+end
